e6a6a9ac1967b09458daa7d12701a9a67d5384fe
Illia Rohozhnyi <rohozhnyiillia@gmail.com>
2025-10-26 18:02:31 +0100

    fix: increased the token time to 15 minutes

--- services/tokenService.js
+++ services/tokenService.js
@@ -5,7 +5,7 @@ class TokenService {
   generateToken(payload) {
     try {
       const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {
-        expiresIn: '3m',
+        expiresIn: '15m',
       })
       const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {
         expiresIn: '7d',
